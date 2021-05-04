5ba75460337c846cb453a6a6ba1a3ea3f3c9dc7a
oguzhannx <oguzhanoztr00@gmail.com>
2021-05-05 00:23:00 +0300

    v1

--- InterfaceAbstractDemo/src/Main.java
+++ InterfaceAbstractDemo/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 	public static void main(String[] args) {
 		//Customer customer = new Customer(1, "asda", "asdasd", new Date(2000, 10, 4), "37018041374");
 		BaseCustomerManager customer = new StarbucksCustomerManager(new MernisServiceAdapter());
-		customer.add(new Customer(1, "Oðuzhan", "Öztürk", new Date(2000, 10, 4), "37018041374"));
+		customer.add(new Customer(1, "Oðuzhan", "Öztürk", new Date(1999, 9, 9), "99999999999"));
 	}
 
 }
